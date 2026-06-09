ff7f1dd0f622776422aaf720b96c6955980c72e7
Bhupendra Singh <singhgamer0050@gmail.com>
2026-06-09 12:52:16 +0530

    Implemented add and view student features

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# First-Project-Student-Manajement-System-
-Making my first project 
+# Student Management System
+
+My first Python project built while learning Python.
+
+## Features
+- Create Student Records
+- View Student Details
+
+## Technologies Used
+- Python
+- OOP
+
+## Future Features
+- Add Student
+- Search Student
+- Delete Student
+- Update Student
+- Save Data
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-student =[]
-class student:
+class Student:
     def __init__(self, name, roll_no, branch, marks):
         self.name = name
         self.roll_no = roll_no
@@ -7,10 +6,38 @@ class student:
         self.marks = marks
 
     def view_stu(self):
+        print("Student details:")
         print("Student name =",self.name )
         print("Student roll no. =",self.roll_no )
         print("Student branch =",self.branch )
         print("Student marks =",self.marks )
 
-stu1 = student("Aman",12,"CSE(AI)",90)
-stu1.view_stu()
+students =[]
+while True:
+    menu = int(input(
+    "1. Add Student\n"
+    "2. View Student\n"
+    "3. Exit\n"
+    "Enter your choice: "
+    ))
+    if menu == 1:
+        name = input("Enter your name:")
+        roll_no = int(input("Enter your roll number:"))
+        branch = input("Enter your Branch:")
+        marks = int(input("Enter your marks:"))
+        print("-" * 20)
+        stu1 = Student(name, roll_no, branch, marks)
+        students.append(stu1)
+    elif menu == 2:
+        if len(students) == 0:
+            print("No students found!")
+        else:
+            for student in students:
+                print("-" * 20)
+                student.view_stu()
+                print("-" * 20)
+    elif menu == 3:
+        print("Thankyou")
+        break
+    else:
+        print("Invalid choise")
