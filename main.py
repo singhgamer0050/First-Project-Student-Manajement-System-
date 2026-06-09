3e27de286eed346ae182ef4cf954c6da60f46a7b
Bhupendra Singh <singhgamer0050@gmail.com>
2026-06-09 11:13:14 +0530

    Define student class

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ class student:
         self.branch = branch
         self.marks = marks
 
-    def add_stu(self):
+    def view_stu(self):
         print("Student name =",self.name )
         print("Student roll no. =",self.roll_no )
         print("Student branch =",self.branch )
         print("Student marks =",self.marks )
 
 stu1 = student("Aman",12,"CSE(AI)",90)
-stu1.add_stu()
+stu1.view_stu()
