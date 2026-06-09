e9a48b063ac7851d3a154b915f939e552c596e35
Bhupendra Singh <singhgamer0050@gmail.com>
2026-06-09 11:01:28 +0530

    Define student class

---
+++ main.py
@@ -0,0 +1,16 @@
+student =[]
+class student:
+    def __init__(self, name, roll_no, branch, marks):
+        self.name = name
+        self.roll_no = roll_no
+        self.branch = branch
+        self.marks = marks
+
+    def add_stu(self):
+        print("Student name =",self.name )
+        print("Student roll no. =",self.roll_no )
+        print("Student branch =",self.branch )
+        print("Student marks =",self.marks )
+
+stu1 = student("Aman",12,"CSE(AI)",90)
+stu1.add_stu()
